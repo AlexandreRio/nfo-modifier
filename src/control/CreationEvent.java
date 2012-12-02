5a54f06ea7670f521395d2cd8cebdc303ba08ba2
Alexandre Rio <orbital.project42@gmail.com>
2012-12-02 21:17:07 +0100

    Modify launcher, correct some graphic stuff, re-generate documentation

---
+++ src/control/CreationEvent.java
@@ -0,0 +1,47 @@
+package control;
+
+import data.RWFile;
+import view.NfoView;
+
+import javax.swing.JTextArea;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Reaction class called at the creation of the application.
+ * @author Rio Alexandre
+ * @version 0.1
+ */
+public abstract class CreationEvent {
+
+   /**
+    * Open and display a file, without asking for saving.
+    * @param filePath Path of the file.
+    * @param view NfoView instance, text will be displayed in its text area.
+    */
+   public static void openSpecifiedFileCreation(String filePath, NfoView theView) {
+      JTextArea textArea = theView.getTextArea();
+      File file;
+      ArrayList<String> lineList;
+      int longestLine = 0;
+      if ( filePath != null ) {
+	 file = new File (filePath);
+	 if ( file.exists() ) {
+	    lineList = RWFile.readNfoFile(file.getAbsolutePath());
+	    for (String line : lineList) {
+	       if (line.length() > longestLine)
+		  longestLine = line.length();
+	       textArea.append(line);
+	    }
+	    if ( longestLine > 80) {
+	       textArea.setColumns(longestLine);
+	       theView.pack();
+	    }
+	    RWFile.setCurrentFile(file.getAbsolutePath());
+	    RWFile.setIsModified(false);
+	    theView.setTitle("Nfo-modifier : " + file.getName());
+	 }
+      }
+   }
+
+}
